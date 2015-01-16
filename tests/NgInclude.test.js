8b860338dc1d33930a9604d4203989c180bd4f61
katarina <golbang.k@gmail.com>
2015-01-16 20:31:10 +0100

    ngInclude test for objects

--- tests/NgInclude.test.js
+++ tests/NgInclude.test.js
@@ -9,11 +9,10 @@ describe('Inspector abilities for ngInclude scope problems', function () {
         module('scope-aware');
     });
 
-    beforeEach(inject(function (_$rootScope_, _$compile_, _$templateCache_, _Inspector_) {
+    beforeEach(inject(function (_$rootScope_, _$compile_, _Inspector_) {
         $rootScope = _$rootScope_;
         $compile = _$compile_;
         Inspector = _Inspector_;
-        _$templateCache_.put('tpl', '<div><input ng-model="primitive"></div>');
     }));
 
     describe('usage of primitives', function () {
@@ -21,6 +20,7 @@ describe('Inspector abilities for ngInclude scope problems', function () {
         var $scope, el, element, Ctrl;
         beforeEach((inject(function ($controller, $templateCache) {
 
+            $templateCache.put('tpl', '<div><input ng-model="primitive"></div>');
             $scope = $rootScope.$new();
             Ctrl = $controller('Ctrl', {
                 '$scope' : $scope
@@ -58,46 +58,45 @@ describe('Inspector abilities for ngInclude scope problems', function () {
         });
     });
 
-    /*
     describe('usage of objects', function () {
-        var $scope, el, element;
-        beforeEach(function () {
-            $scope = $rootScope.$new();
-            $scope.val = {};
-            $scope.values = [{key: 'a'}, {key: 'b'}, {key: 'c'}];
 
-            var tpl = '<ul><li ng-repeat="val in values"><input type="text" ng-model="val.key"><p>{{key}}</p></li></ul>';
+        var $scope, el, element, Ctrl;
+        beforeEach((inject(function ($controller, $templateCache) {
+            $templateCache.put('tpl', '<div><input ng-model="primitive.key"></div>');
+            $scope = $rootScope.$new();
+            Ctrl = $controller('Ctrl', {
+                '$scope' : $scope
+            });
+            $scope.primitive = { key : 'a' };
+
+            var tpl = "<div><div ng-include=\" 'tpl' \"></div></div>";
             el = angular.element(tpl);
             element = $compile(el)($scope);
-            angular.element(document.body).append(element);
             $scope.$digest();
-        });
+        })));
+
         afterEach(function() {
             console.log(Inspector.inspect($scope));
         });
 
         it("doesnt change the original value",function() {
-            var input = el.find('input')[0];
+            var input = element.find('input')[0];
             var scope = angular.element(input).scope();
             angular.element(input).val('AAA').triggerHandler('input');
             $scope.$digest();
 
-            //scopes are still different
+            //scopes are different
             expect(scope).not.toBe($scope);
             expect(scope.$parent).toBe($scope);
 
-            //the value in the parent scope is 'AAA'
-            expect(scope.val.key).toBe('AAA');
-            expect($scope.values[0]).toEqual(jasmine.objectContaining({key: 'AAA'}));
+            //the value in the parent scope is changed as well
+            expect(scope.primitive.key).toBe('AAA');
+            expect($scope.primitive.key).toBe('AAA');
 
             //additional tests (ng-scope-aware)
-            //.. creates an own member 'val'
-            expect(scope).toHaveMembers(['val']);
-            //.. inherits 'values' but not 'val'
-            expect(scope).toHaveInheritedMembers(['values']);
-            //NOTE: the test not.toShadow(['val']) doesn't make any sense
-            //because ng-repeat behaves independently no matter whether val is Object or a primitive
-            //this works as designed.
+            expect(scope).toHaveMembers(['primitive']);
+            expect(scope).toHaveInheritedMembers(['primitive']);
+            expect(scope).not.toShadow(['primitive']);
         });
-    });*/
+    });
 });
